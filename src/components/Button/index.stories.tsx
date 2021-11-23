0d2f58cac720ab6999049c7e4535d6622f80a431
“LaurentDellaNegra” <laurent.dellanegra@gmail.com>
2021-11-23 23:03:53 +0100

    Add figma prototype link

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
 import Button, { ButtonProps } from "./index";
+import { withDesign } from 'storybook-addon-designs'
 
 export default {
   title: "Components/Button",
@@ -9,6 +10,7 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  decorators: [withDesign],
 } as Meta;
 
 // Create a master template for mapping args to render the Button component
@@ -17,6 +19,18 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 // Reuse that template for creating different stories
 export const Primary = Template.bind({});
 Primary.args = { label: "Primary 😃", size: "large" };
+Primary.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/ZPaISlA0sMryYWfrFAwDap/Terre-Rouge?node-id=8%3A2',
+  },
+}
 
 export const Secondary = Template.bind({});
 Secondary.args = { ...Primary.args, primary: false, label: "Secondary 😇"};
+Secondary.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/ZPaISlA0sMryYWfrFAwDap/Terre-Rouge?node-id=8%3A2',
+  },
+}
